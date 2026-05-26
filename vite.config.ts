b89e2472ad935b0b825a1e3fa8d31facee79883a
Nathisooriya S <nathisooriya15@gmail.com>
2026-05-26 13:46:17 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  base: "/",
+  plugins: [
+    viteTsConfigPaths({ projects: ["./tsconfig.json"] }),
+    tailwindcss(),
+    tanstackStart(),
+    nitro(),
+    viteReact(),
+  ],
+});
